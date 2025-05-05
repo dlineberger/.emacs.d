d3ffd3b37f2f4918afa7b43f173d6e59c3552b6e
David Lineberger <dlineberger@m2-macbook-air.local>
2025-05-05 16:22:32 -0700

    Default frame alist

--- early-init.el
+++ early-init.el
@@ -1,7 +1,9 @@
 (setenv "LSP_USE_PLISTS" "true")
 
-(setq initial-frame-alist
+(setq default-frame-alist
        '((height . 0.86)
          (width . 0.48)
          (left + 10)
-         (top + 30)))
+         (top + 30)
+	 (vertical-scroll-bars . nil)
+	 (tool-bar-lines . 0)))
